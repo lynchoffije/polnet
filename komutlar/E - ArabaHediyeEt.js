3269de863a96735e82171e2ce0e841d54c5be413
Glitch (craveobot) <none>
2023-09-23 18:13:00 +0000

    🌘🚤 Checkpoint ./komutlar/E - ArabaHediyeEt.js:61608367/1455

--- komutlar/E - ArabaHediyeEt.js
+++ komutlar/E - ArabaHediyeEt.js
@@ -14,7 +14,8 @@ exports.run = (client, message, params) => {
     "Audi RS 7": "https://i.ytimg.com/vi/jwC061bKG2Y/maxresdefault.jpg",
     "Nissan GT-R": "https://cdn.motor1.com/images/mgl/rMjOr/s1/2022-nissan-gt-r-nismo-special-edition.jpg",
     "TOGG T10X": "https://img.piri.net/mnresize/712/-/resim/imagecrop/2022/11/01/10/34/resized_b9266-9c2dd2e0toggdunyamansetlerinderusyadanabdyecindenyunanistanaherkesovguyagdirdi1667121343392.jpg",
-    "Tofaş Doğan SLX": ""TOGG T10X": "https://img.piri.net/mnresize/712/-/resim/imagecrop/2022/11/01/10/34/resized_b9266-9c2dd2e0toggdunyamansetlerinderusyadanabdyecindenyunanistanaherkesovguyagdirdi1667121343392.jpg",",
+    "Tofaş Doğan SLX": "https://s0.smartresize.com/wallpaper/541/830/HD-wallpaper-unal-turan-tofas-34vtf58-dogan-farzetki-matkaps-modifiye-sahin-slx-tofas-tofask.jpg",
+    "Rolls Royce Cullinan": "https://i0.wp.com/safencars.com/wp-content/uploads/2022/12/DSC_8259-scaled.jpg",
   };
 
   let randomMarka = Object.keys(arabalar)[Math.floor(Math.random() * Object.keys(arabalar).length)];
@@ -28,7 +29,7 @@ exports.run = (client, message, params) => {
   }
 
   const arabaver = new Discord.MessageEmbed()
-    .setDescription(`${message.author} ${user} Kişisine **Vegga** Galeriden **${randomMarka}** Hediye Etti. 😱 Ama 1 Sana Birde Devlete 🙄`)
+    .setDescription(`${message.author} ${user} Kişisine **Vegga** Galeriden **${randomMarka}** Hediye Etti. 😱`)
     .setImage(arabagorsel)
     .setColor("BLACK")
     .setFooter(
